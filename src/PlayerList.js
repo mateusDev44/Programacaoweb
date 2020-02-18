4d818564c2f9096cbc31f83fd6ca9a907f765e53
Aluno Lab. 03 <aluno03@aeelabs.edu.br>
2020-02-17 21:47:49 -0300

    ajustes

---
+++ src/PlayerList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PlayerList = ()=> {
+    return (
+        <div>
+            <h1>Pedro Carrinjo</h1>
+            <h2>Jaragua</h2>
+            <h3>Brasil</h3>
+        </div>
+    );
+};
+
+export default PlayerList;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
 import React from 'react';
-import RactDom from 'react-dom';
+import ReactDOM from 'react-dom';
+import PlayerList from './PlayerList';
+
+const App = () => {
+    const textBt = "Me chupa";
+    return (
+        <div>
+            <section style={{ display: "flex", justifyContent: "center" }}>
+                <form style={{ display: "flex", flexDirection: "column", alignItems: "center", width: "80%" }} method="POST">
+                    <label>Nome</label>
+                    <input style={{ borderRadius: "7px", border: "1px solid red", outline: "none", width: "100%", paddingLeft: "15px" }} />
+                    <button style={{ border: "none", borderRadius: "14px", margin: "5px 0", backgroundColor: "red", color: "white", height: "34px", fontSize: "24px", width: "100%" }}>{textBt}</button>
+                </form>
+            </section>
+            <PlayerList />
+        </div>
+    );
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"))
