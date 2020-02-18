b508a334fa309d30ffa1556c8e690c099697bb43
Aluno Lab. 03 <aluno03@aeelabs.edu.br>
2020-02-17 22:09:36 -0300

    ajustes

--- src/PlayerList.js
+++ src/PlayerList.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
-const PlayerList = ()=> {
-    return (
-        <div>
-            <h1>Pedro Carrinjo</h1>
-            <h2>Jaragua</h2>
-            <h3>Brasil</h3>
-        </div>
-    );
+const PlayerList = (props)=> {
+    console.log(props.tropa.find(soldado => soldado.nome === "Pedro Felipe"))
+    let list = [];
+    props.tropa.forEach(soldado => {
+        list.push(
+            <div>
+                <h1>{soldado.nome}</h1>
+                <h2>{soldado.posicao}</h2>
+                <h3>{soldado.codeNome}</h3>
+            </div>
+        )
+    })
+    return list;
 };
 
 export default PlayerList;
--- src/index.js
+++ src/index.js
@@ -3,17 +3,32 @@ import ReactDOM from 'react-dom';
 import PlayerList from './PlayerList';
 
 const App = () => {
-    const textBt = "Me chupa";
+    const tropaGay = [
+        {
+            nome: "Pedro Felipe",
+            posicao: "de 4",
+            codeNome: "Bumbum de Veludo"
+        },
+        {
+            nome: "Dreykson",
+            posicao: "Mamae e pai",
+            codeNome: "Boca de ouro"
+        }
+    ]
     return (
         <div>
             <section style={{ display: "flex", justifyContent: "center" }}>
                 <form style={{ display: "flex", flexDirection: "column", alignItems: "center", width: "80%" }} method="POST">
                     <label>Nome</label>
                     <input style={{ borderRadius: "7px", border: "1px solid red", outline: "none", width: "100%", paddingLeft: "15px" }} />
-                    <button style={{ border: "none", borderRadius: "14px", margin: "5px 0", backgroundColor: "red", color: "white", height: "34px", fontSize: "24px", width: "100%" }}>{textBt}</button>
+                    <label>Posicao</label>
+                    <input style={{ borderRadius: "7px", border: "1px solid red", outline: "none", width: "100%", paddingLeft: "15px" }} />
+                    <label>CodeNome</label>
+                    <input style={{ borderRadius: "7px", border: "1px solid red", outline: "none", width: "100%", paddingLeft: "15px" }} />
+                    <button style={{ border: "none", borderRadius: "14px", margin: "5px 0", backgroundColor: "red", color: "white", height: "34px", fontSize: "24px", width: "100%" }}>Me Chupa</button>
                 </form>
             </section>
-            <PlayerList />
+            <PlayerList tropa={tropaGay}/>
         </div>
     );
 }
